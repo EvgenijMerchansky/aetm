c88694f409e9725be0cb42dded138d2edd343ebb
evgenijMerchansky <evgenij.merchansky@gmail.com>
2017-12-08 14:12:11 +0200

    change store.js file

--- src/store.js
+++ src/store.js
@@ -1,12 +1,12 @@
 import { createStore } from 'redux';
 import { applyMiddleware } from 'redux';
-import { logger } from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+// import { logger } from 'redux-logger'; // if you want look for actions
 
 import userReducer from './Reducers/userReducer';
 
-const middleware = applyMiddleware(logger, thunk);
+const middleware = applyMiddleware(thunk);
 const store = createStore(userReducer, composeWithDevTools(middleware));
 
 export default store;
